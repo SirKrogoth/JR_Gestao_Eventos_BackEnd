355f68f9122d0187cbd6a1563d2250096d86800d
João Rafael Menezes <menezes.jrafael@gmail.com>
2024-04-27 02:45:02 -0300

    Agora vai

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+
+(async () =>{
+    try {
+        
+    } catch (error) {
+        console.error('Fail to connect database, contact the administrator and show the message. Message: ' + error);
+    }
+})();
